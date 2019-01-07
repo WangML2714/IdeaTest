d4415d17104d3ae584760f964813732ac5ede632
Wangml <WangMl0207@163.com>
2019-01-08 00:20:20 +0800

    添加Test测试工程到git

---
+++ idea-git-test/src/com/itcast/Test.java
@@ -0,0 +1,12 @@
+package com.itcast;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("好嗨呦");
+        System.out.println("好嗨呦");
+        System.out.println("好嗨呦");
+        System.out.println("好嗨呦");
+        System.out.println("好嗨呦");
+        System.out.println("好嗨呦");
+    }
+}
